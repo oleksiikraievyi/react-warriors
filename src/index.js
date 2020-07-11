0c85b29b777e37198cac02d053b8fd704b5d4598
Oleksii Kraievyi <okraievyi@emergetms.com>
2020-07-11 19:37:44 +0300

    move App component to separarte class

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import MovieItem from "./components/MovieItem"
-
-let movie = {
-    title : "Avengers : Endgame",
-    rating : 9.0,
-    overview : "Last movie in Avengers franchise",
-    image : "https://images-na.ssl-images-amazon.com/images/I/71tl2F9GRxL._AC_SL1000_.jpg"
-};
-
-function App() {
-    return (
-        <div>
-            <MovieItem data={movie}/>
-        </div>
-    )
-}
+import App from './components/App'
 
 ReactDOM.render(<App />, document.getElementById('root'));
